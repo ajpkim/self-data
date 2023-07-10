1afcb5b01b2087f78993ac7b40d98fb1a3bf869a
Alex Kim <dev@ajpkim.com>
2023-07-10 15:32:22 -0400

    Init habits, supabase, navbar

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="py-4">
+      <Link className="mr-4" href="/">
+        Home
+      </Link>
+      <Link className="mr-4" href="/habits">
+        Habits
+      </Link>
+    </nav>
+  )
+}
---
+++ src/app/habits/page.tsx
@@ -0,0 +1,9 @@
+import supabase from '@/api'
+
+export default function Habits() {
+  return (
+    <>
+      <p>HABITS YO</p>
+    </>
+  )
+}
